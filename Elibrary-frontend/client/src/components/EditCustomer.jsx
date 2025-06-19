e9b30c6ac96b9c7e667075d766df3a3143172496
HariPrasathM2005 <mo.hariprasath@gmail.com>
2025-06-19 15:46:37 +0530

    Add all files

---
+++ Elibrary-frontend/client/src/components/EditCustomer.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import CustomerForm from '../components/CustomerForm';
+
+function EditCustomer() {
+    const { id } = useParams();
+    const [customer, setCustomers] = useState(null);
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/api/customers/${id}`).then((res) => {
+            if(!res.ok) throw new Error("Failed to fetch customer data");
+            return res.json();
+        }).then((data) => {
+            setCustomers(data);
+        }).catch((error) => {
+            console.error("Error fetching customer data:", error);
+            alert("Failed to fetch customer data. Please try again.");
+        })
+    }, [id]);
+    return (
+        <div>
+            {customer && <CustomerForm initialData={customer} />}
+        </div>
+    )
+}
+
+export default EditCustomer;
